e50a308b18f7aaa812870ca1bdac1ba499bfbb90
Xwg <loloxwg@gmail.com>
2023-06-25 10:29:34 +0800

    Add simple CI for go (#5)
    * Add unimplemented manifest method
    
    * Change gitignore and add CI for go
    
    * Change CI including paths

--- go/internal/manifest/manifest.go
+++ go/internal/manifest/manifest.go
@@ -48,3 +48,27 @@ type ManifestV2 struct {
 func (m *ManifestV2) Schema() *arrow.Schema {
 	return m.schema
 }
+
+func (m *ManifestV2) AddScalarDataFiles(files ...*DataFile) {
+	m.files = append(m.files, files...)
+
+}
+
+func (m *ManifestV2) AddVectorDataFiles(files ...*DataFile) {
+	m.files = append(m.files, files...)
+}
+
+func (m *ManifestV2) ScalarSchema() *arrow.Schema {
+	panic("implement me")
+	return nil
+}
+
+func (m *ManifestV2) VectorSchema() *arrow.Schema {
+	panic("implement me")
+	return nil
+}
+
+func WriteManifestV2File(fs fs.Fs, manifest *ManifestV2) error {
+	panic("implement me")
+	return nil
+}
