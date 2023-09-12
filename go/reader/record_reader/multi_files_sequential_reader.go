a5bba27c2a2e97ebc22c7fc90d68a9d8d7969f09
Bingyi Sun <sunbingyi1992@gmail.com>
2023-09-12 15:43:12 +0800

    Add functions to create arrow reader (#54)
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- go/reader/record_reader/multi_files_sequential_reader.go
+++ go/reader/record_reader/multi_files_sequential_reader.go
@@ -1,38 +1,40 @@
 package record_reader
 
 import (
+	"sync/atomic"
+
 	"github.com/apache/arrow/go/v12/arrow"
-	"github.com/apache/arrow/go/v12/arrow/array"
 	"github.com/milvus-io/milvus-storage/go/file/fragment"
-	"github.com/milvus-io/milvus-storage/go/io/format"
+	"github.com/milvus-io/milvus-storage/go/io/format/parquet"
 	"github.com/milvus-io/milvus-storage/go/io/fs"
 	"github.com/milvus-io/milvus-storage/go/storage/options/option"
 )
 
 type MultiFilesSequentialReader struct {
-	fs                fs.Fs
-	schema            *arrow.Schema
-	files             []string
-	nextPos           int
-	currReader        array.RecordReader
-	holdingFileReader format.Reader
-	err               error
-	options           *option.ReadOptions
+	fs         fs.Fs
+	schema     *arrow.Schema
+	files      []string
+	nextPos    int
+	options    *option.ReadOptions
+	currReader *parquet.FileReader
+	err        error
+	ref        int64
 }
 
 func (m MultiFilesSequentialReader) Retain() {
-	//TODO implement me
-	panic("implement me")
+	atomic.AddInt64(&m.ref, 1)
 }
 
 func (m MultiFilesSequentialReader) Release() {
-	//TODO implement me
-	panic("implement me")
+	if atomic.AddInt64(&m.ref, -1) == 0 {
+		if m.currReader != nil {
+			m.currReader.Close()
+		}
+	}
 }
 
 func (m MultiFilesSequentialReader) Schema() *arrow.Schema {
-	//TODO implement me
-	panic("implement me")
+	return m.schema
 }
 
 func (m MultiFilesSequentialReader) Next() bool {
@@ -46,8 +48,7 @@ func (m MultiFilesSequentialReader) Record() arrow.Record {
 }
 
 func (m MultiFilesSequentialReader) Err() error {
-	//TODO implement me
-	panic("implement me")
+	return m.err
 }
 
 func NewMultiFilesSequentialReader(fs fs.Fs, fragments fragment.FragmentVector, schema *arrow.Schema, options *option.ReadOptions) *MultiFilesSequentialReader {
@@ -62,5 +63,6 @@ func NewMultiFilesSequentialReader(fs fs.Fs, fragments fragment.FragmentVector, 
 		options: options,
 		files:   files,
 		nextPos: 0,
+		ref:     1,
 	}
 }
