0d54a8c7f8f4e535daa206529a09a58434834d36
Bingyi Sun <sunbingyi1992@gmail.com>
2023-09-25 15:41:19 +0800

    Change lock manager interfaces (#61)
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- go/storage/lock/lock_manager.go
+++ go/storage/lock/lock_manager.go
@@ -4,15 +4,15 @@ import "github.com/milvus-io/milvus-storage/go/common/constant"
 
 type LockManager interface {
 	// Acquire the lock, wait until the lock is available, return the version to be modified or use the newest version
-	Acquire() (version int64, useLatestVersion bool)
+	Acquire() (version int64, useLatestVersion bool, err error)
 	// Release the lock, accepts the new allocated manifest version and success state of operations between Acquire and Release as parameters
-	Release(version int64, success bool)
+	Release(version int64, success bool) error
 }
 
 type EmptyLockManager struct{}
 
-func (h *EmptyLockManager) Acquire() (version int64, useLatestVersion bool) {
-	return constant.LatestManifestVersion, true
+func (h *EmptyLockManager) Acquire() (version int64, useLatestVersion bool, err error) {
+	return constant.LatestManifestVersion, true, nil
 }
 
-func (h *EmptyLockManager) Release(_ int64, _ bool) {}
+func (h *EmptyLockManager) Release(_ int64, _ bool) error { return nil }
