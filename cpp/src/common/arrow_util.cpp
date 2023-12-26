76e506e3ae48ff1ee7239d71c414390882a35e6b
Bingyi Sun <sunbingyi1992@gmail.com>
2023-12-26 11:34:37 +0800

    [Go]: fix transactions (#98)
    Signed-off-by: sunby <sunbingyi1992@gmail.com>

--- cpp/src/common/arrow_util.cpp
+++ cpp/src/common/arrow_util.cpp
@@ -1,11 +1,11 @@
 // Copyright 2023 Zilliz
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,7 +72,6 @@ Result<std::shared_ptr<arrow::RecordBatchReader>> MakeArrowRecordBatchReader(
 
   // RETURN_ARROW_NOT_OK(reader->GetRecordBatchReader(row_group_indices, column_indices, &record_reader));
   RETURN_ARROW_NOT_OK(reader->GetRecordBatchReader(row_group_indices, &record_reader));
-  LOG_STORAGE_INFO_ << "metadata for batch reader:" << record_reader->schema()->ToString(true);
   return record_reader;
 }
 
