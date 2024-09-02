0bf2d0537da9f9ef3f60e87ab0bab4a40b42408f
sthuang <167743503+shaoting-huang@users.noreply.github.com>
2024-09-02 15:36:55 +0800

    add random data for ut (#140)
    issue: #127
    
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- cpp/test/packed/packed_test_base.h
+++ cpp/test/packed/packed_test_base.h
@@ -20,8 +20,10 @@
 #include <arrow/record_batch.h>
 #include <arrow/builder.h>
 #include <memory>
-#include "test_util.h"
 #include "arrow/table.h"
+#include "common/log.h"
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -30,41 +32,60 @@ namespace milvus_storage {
 class PackedTestBase : public ::testing::Test {
   protected:
   void SetUpCommonData() {
+    record_batch_ = randomRecordBatch();
+    table_ = arrow::Table::FromRecordBatches({record_batch_}).ValueOrDie();
+    schema_ = table_->schema();
+  }
+
+  protected:
+  std::shared_ptr<arrow::RecordBatch> randomRecordBatch() {
     arrow::Int32Builder int_builder;
     arrow::Int64Builder int64_builder;
     arrow::StringBuilder str_builder;
 
-    ASSERT_STATUS_OK(int_builder.AppendValues(int32_values));
-    ASSERT_STATUS_OK(int64_builder.AppendValues(int64_values));
-    ASSERT_STATUS_OK(str_builder.AppendValues(str_values));
+    int32_values = {rand() % 10000, rand() % 10000, rand() % 10000};
+    int64_values = {rand() % 10000000, rand() % 10000000, rand() % 10000000};
+    str_values = {random_string(10000), random_string(10000), random_string(10000)};
+
+    int_builder.AppendValues(int32_values);
+    int64_builder.AppendValues(int64_values);
+    str_builder.AppendValues(str_values);
 
     std::shared_ptr<arrow::Array> int_array;
     std::shared_ptr<arrow::Array> int64_array;
     std::shared_ptr<arrow::Array> str_array;
 
-    ASSERT_STATUS_OK(int_builder.Finish(&int_array));
-    ASSERT_STATUS_OK(int64_builder.Finish(&int64_array));
-    ASSERT_STATUS_OK(str_builder.Finish(&str_array));
+    int_builder.Finish(&int_array);
+    int64_builder.Finish(&int64_array);
+    str_builder.Finish(&str_array);
 
-    std::vector<std::shared_ptr<arrow::Field>> fields = {arrow::field("int32", arrow::int32()),
-                                                         arrow::field("int64", arrow::int64()),
-                                                         arrow::field("str", arrow::utf8())};
     std::vector<std::shared_ptr<arrow::Array>> arrays = {int_array, int64_array, str_array};
+    auto schema = arrow::schema({arrow::field("int32", arrow::int32()), arrow::field("int64", arrow::int64()),
+                                 arrow::field("str", arrow::utf8())});
+    return arrow::RecordBatch::Make(schema, 3, arrays);
+  }
 
-    schema_ = arrow::schema(fields);
-    record_batch_ = arrow::RecordBatch::Make(schema_, 3, arrays);
-
-    table_ = arrow::Table::FromRecordBatches({record_batch_}).ValueOrDie();
+  std::string random_string(size_t length) {
+    auto randchar = []() -> char {
+      const char charset[] =
+          "0123456789"
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+          "abcdefghijklmnopqrstuvwxyz";
+      const size_t max_index = (sizeof(charset) - 1);
+      return charset[rand() % max_index];
+    };
+    std::string str(length, 0);
+    std::generate_n(str.begin(), length, randchar);
+    return str;
   }
 
   std::shared_ptr<arrow::Schema> schema_;
   std::shared_ptr<arrow::RecordBatch> record_batch_;
   std::shared_ptr<arrow::Table> table_;
 
-  const std::vector<int32_t> int32_values = {1, 2, 3};
-  const std::vector<int64_t> int64_values = {4, 5, 6};
-  const std::vector<basic_string<char>> str_values = {std::string(10000, 'a'), std::string(10000, 'b'),
-                                                      std::string(10000, 'c')};
+  std::vector<int32_t> int32_values;
+  std::vector<int64_t> int64_values;
+  std::vector<basic_string<char>> str_values;
 };
 
 }  // namespace milvus_storage
