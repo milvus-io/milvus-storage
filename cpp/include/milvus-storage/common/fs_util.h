65c150599dfa14b637606c5317d5b3d675fa31b3
shaoting-huang <shaoting.huang@zilliz.com>
2024-09-06 14:50:51 +0800

    byte array kv meta for packed
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- cpp/include/milvus-storage/common/fs_util.h
+++ cpp/include/milvus-storage/common/fs_util.h
@@ -30,4 +30,24 @@ static constexpr int64_t ARROW_PART_UPLOAD_SIZE = 10 * 1024 * 1024;  // 10 MB
 
 static constexpr int64_t MIN_BUFFER_SIZE_PER_FILE = DEFAULT_MAX_ROW_GROUP_SIZE + ARROW_PART_UPLOAD_SIZE;
 
+static const std::string ROW_GROUP_SIZE_META_KEY = "row_group_size";
+
+class PackedMetaSerde {
+  public:
+  // Serialize a vector of size_t to a byte array and convert it to a string
+  static std::string serialize(const std::vector<size_t>& sizes) {
+    std::vector<uint8_t> byteArray(sizes.size() * sizeof(size_t));
+    std::memcpy(byteArray.data(), sizes.data(), byteArray.size());
+    return std::string(byteArray.begin(), byteArray.end());
+  }
+
+  // Deserialize a string back to a vector of size_t
+  static std::vector<size_t> deserialize(const std::string& input) {
+    std::vector<uint8_t> byteArray(input.begin(), input.end());
+    std::vector<size_t> sizes(byteArray.size() / sizeof(size_t));
+    std::memcpy(sizes.data(), byteArray.data(), byteArray.size());
+    return sizes;
+  }
+};
+
 }  // namespace milvus_storage
