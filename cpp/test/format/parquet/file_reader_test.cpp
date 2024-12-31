68f9e38fe72ac9f92eb257ba08e930ca2bf2ab03
shaoting-huang <shaoting.huang@zilliz.com>
2024-12-31 11:22:27 +0800

    file record batch reader
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- cpp/test/format/parquet/file_reader_test.cpp
+++ cpp/test/format/parquet/file_reader_test.cpp
@@ -12,11 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "packed_test_base.h"
-
+#include "../../packed/packed_test_base.h"
+#include "format/parquet/file_reader.h"
 namespace milvus_storage {
 
+class FileReaderTest : public PackedTestBase {};
+
 TEST_F(FileReaderTest, FileRecordBatchReader) {
+  int batch_size = 100;
+
+  PackedRecordBatchWriter writer(writer_memory_, schema_, *fs_, file_path_, storage_config_, props_);
+  for (int i = 0; i < batch_size; ++i) {
+    EXPECT_TRUE(writer.Write(record_batch_).ok());
+  }
+  EXPECT_TRUE(writer.Close().ok());
+
   std::vector<std::shared_ptr<arrow::Field>> fields = {
       arrow::field("int32", arrow::int32()),
       arrow::field("int64", arrow::int64()),
@@ -34,9 +44,9 @@ TEST_F(FileReaderTest, FileRecordBatchReader) {
 
   // read all row groups
   path = file_path_ + "/0";
-  MemRecordBatchReader fr(*fs_, path, schema, reader_memory_);
-  ASSERT_AND_ARROW_ASSIGN(auto m_table, mr.ToTable());
-  ASSERT_STATUS_OK(mr.Close());
+  FileRecordBatchReader fr(*fs_, path, schema, reader_memory_);
+  ASSERT_AND_ARROW_ASSIGN(auto fr_table, fr.ToTable());
+  ASSERT_STATUS_OK(fr.Close());
 
   std::set<int> needed_columns = {0, 1, 2};
   std::vector<ColumnOffset> column_offsets = {
@@ -44,18 +54,17 @@ TEST_F(FileReaderTest, FileRecordBatchReader) {
       ColumnOffset(0, 1),
       ColumnOffset(0, 2),
   };
-  std::vector<std::string> paths = {file_path_ + "/0"};
-  PackedRecordBatchReader pr(*fs_, paths, schema, column_offsets, needed_columns, reader_memory_);
-  ASSERT_AND_ARROW_ASSIGN(auto p_table, pr.ToTable());
+  PackedRecordBatchReader pr(*fs_, {path}, schema, column_offsets, needed_columns, reader_memory_);
+  ASSERT_AND_ARROW_ASSIGN(auto pr_table, pr.ToTable());
   ASSERT_STATUS_OK(pr.Close());
-  ASSERT_EQ(m_table->num_rows(), p_table->num_rows());
+  ASSERT_EQ(fr_table->num_rows(), pr_table->num_rows());
 
   // read row group 1
   path = file_path_ + "/0";
-  MemRecordBatchReader fr2(*fs_, path, schema, reader_memory_, 1, 1);
-  ASSERT_AND_ARROW_ASSIGN(auto rg_table, mr2.ToTable());
-  ASSERT_STATUS_OK(fr2.Close());
-  ASSERT_GT(m_table->num_rows(), rg_table->num_rows());
+  FileRecordBatchReader rgr(*fs_, path, schema, reader_memory_, 1, 1);
+  ASSERT_AND_ARROW_ASSIGN(auto rg_table, rgr.ToTable());
+  ASSERT_STATUS_OK(rgr.Close());
+  ASSERT_GT(fr_table->num_rows(), rg_table->num_rows());
 }
 
 }  // namespace milvus_storage
