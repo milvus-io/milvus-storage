f206f4903a80d0434bca6994d0b822b27abe6ed5
shaoting-huang <shaoting.huang@zilliz.com>
2025-08-09 11:50:17 +0800

    wrap cloud object storage test
    Signed-off-by: shaoting-huang <shaoting.huang@zilliz.com>

--- cpp/src/filesystem/azure/azure_fs.cpp
+++ cpp/src/filesystem/azure/azure_fs.cpp
@@ -25,16 +25,13 @@ namespace milvus_storage {
 
 Result<ArrowFileSystemPtr> AzureFileSystemProducer::Make() {
   arrow::fs::AzureOptions options;
-  arrow::util::Uri uri_parser;
-  auto uri = options.blob_storage_scheme + "://" + config_.bucket_name + options.blob_storage_authority;
-  RETURN_ARROW_NOT_OK(uri_parser.Parse(uri));
   assert(!config_.access_key_id.empty());
   options.account_name = config_.access_key_id;
   if (config_.useIAM) {
     assert(getenv("AZURE_CLIENT_ID") != NULL);
     assert(getenv("AZURE_TENANT_ID") != NULL);
     assert(getenv("AZURE_FEDERATED_TOKEN_FILE") != NULL);
-    options.ConfigureWorkloadIdentityCredential();
+    RETURN_ARROW_NOT_OK(options.ConfigureWorkloadIdentityCredential());
   } else {
     // need azure secret key without iam
     assert(!config_.access_key_value.empty());
@@ -42,7 +39,7 @@ Result<ArrowFileSystemPtr> AzureFileSystemProducer::Make() {
   }
 
   ASSIGN_OR_RETURN_ARROW_NOT_OK(auto fs, arrow::fs::AzureFileSystem::Make(options));
-  fs->CreateDir(config_.root_path, true);
+  RETURN_ARROW_NOT_OK(fs->CreateDir(config_.root_path, true));
   return ArrowFileSystemPtr(fs);
 }
 
